3f428c939349d6784ad11ab1da3611f0961cc3ca
Claire Peterson <40612126+petercla0119@users.noreply.github.com>
2024-11-18 19:22:28 -0600

    comit all local changes on claireps profile

---
+++ .anaconda/navigator/scripts/anaconda-toolbox.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /opt/anaconda3/bin/activate /opt/anaconda3
+jupyter-lab --LabApp.default_url="/lab?open_assistant=true&reset" >/Users/claireps/.anaconda/navigator/scripts/anaconda-toolbox-out-1.txt 2>/Users/claireps/.anaconda/navigator/scripts/anaconda-toolbox-err-1.txt
